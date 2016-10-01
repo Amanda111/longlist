405a94d0874d400428efa00258228c0555bda747
Amanda111 <631551773@qq.com>
2016-10-01 23:56:01 +0800

    longlist

--- js/app.vue
+++ js/app.vue
@@ -1,13 +1,19 @@
 <template>
-	<div>
-		<ul v-for="li in list" track-by="$index">
-			<li>${ $index }</li>
-		</ul>
-	</div>
+<div class="box" v-el:box>
+	<div class="top" v-bind:style="{ height: toph + 'px' }"></div>
+	<table>
+		<tr v-for="li in list | show " track-by="$index">
+			<td>${ $index }</td>
+		</tr>
+	</table>
+	<div class="bottom" v-bind:style="{ height: bottomh + 'px' }"></div>
+</div>
 </template>
 <style>
-	li{
-		list-style: none;
+	.box{
+		width: 500px;
+		height: 200px;
+		overflow: scroll;
 	}
 </style>
 <script>
@@ -15,11 +21,57 @@
 	export default {
 		data (){
 			return {
-				list:['1','2','3','4','5','6','7','8']
+				list:[],
+				scroll:0,
+				toph:0,
+				bottomh:0,
+				scrollh:0
+			}
+		},
+		created: function(){
+			for (var i = 0; i < 500; i++) {
+				this.list.push({text:"1",show:true})
 			}
 		},
 		methods:{
-
+			handleScroll () {
+				this.scroll = this.$els.box.scrollTop
+				// var scrollh = this.$els.box.scrollHeight
+				var boxh = this.$els.box.clientHeight
+				var tdh = document.getElementsByTagName('td')[0].offsetHeight
+				var num = this.scroll / this.scrollh * this.list.length
+				var no = Math.ceil(num - 0.5)
+				console.log(no)
+				this.toph = num * tdh
+				this.bottomh = this.scrollh - this.toph - tdh * 10
+				var  self = this
+    			self.list.forEach(function(li){
+        			li.show = false;
+      			})
+	      		self.$nextTick(function(){
+	      			self.list[no].show = true
+					// for(var i = 0;i < 500;i++){
+					// 	(function(){
+					// 		if(i >= no  && i < no + 10){
+					// 			self.list[i].show = true
+					// 		}else{
+					// 			self.list[i].show = false
+					// 		}
+					// 	})(i)
+					}
+	            });
+			}
+		},
+		filters: {
+	        show: function(list) {
+	            return list.filter(function(li) {
+	                return li.show;
+	            });
+	        },
+  		},
+		ready () {
+			this.$els.box.addEventListener('scroll', this.handleScroll);
+			this.scrollh = this.$els.box.scrollHeight
 		}
 	}
 </script>
