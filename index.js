b3887442a5b0f114deca2c2cb0dbdfc7a3bd8df2
Amanda111 <631551773@qq.com>
2016-10-21 20:55:56 +0800

    branch

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-import Vue from 'vue'
-import app from './src/infinite.vue'
+var vueInfiniteList = require('./src/infinite.vue');
 
-//listData component
-var listData = Vue.component('item', {
-	props:['listdata', 'dataH'],
-	template: '<div>{{listdata}}</div>'
-})
-
-//loding block component
-var loding = Vue.component('loding', {
-	props:['lodingH'],
-	template: '<div>loding</div>'
-})
-
-var infinite = {
-	mixins:[app],
-	components:{
-		item: listData,
-		loding:loding
-	}
-}
-new Vue({
-	el: "body",
-	data:{
-		dataH:50,
-		lodingH:30,
-		url:'/api',
-		no:20,
-		num:80
-	},
-	components:{scroll:infinite}
-})
+module.exports = vueInfiniteList;
 
